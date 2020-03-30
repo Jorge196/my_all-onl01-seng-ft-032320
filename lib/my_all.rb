a8c49d7b5df0c5246542dad803bc674464f2b6b9
Jorge Ramirez <joramirez96@gmail.com>
2020-03-30 22:41:28 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,11 +1,14 @@
-require 'pry'
-
 def my_all?(collection)
   i = 0
-  binding.pry
+  block_return_values = []
   while i < collection.length
-    yield (collection[i])
+    block_return_yield << (collection[i])
     i = i + 1
   end
 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
