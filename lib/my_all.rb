ee4b8b7c6003060d2a189b1f4673391b44d535ea
Jorge Ramirez <joramirez96@gmail.com>
2020-03-30 22:45:28 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,7 +2,7 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.size
-    block_return_yield.push (yield(collection[i])
+    block_return_yield.push (yield(collection[i]))
     i += 1
   end
 
