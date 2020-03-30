bfbfa64321aeecfa41518ac8a2fb24e4bf302284
Jorge Ramirez <joramirez96@gmail.com>
2020-03-30 22:58:17 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,10 +4,10 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
-    block_return_yield << yield(collection[i])
+    return false if !yield(collection[i])
     i += 1
   end
-    puts block_return_values
+    true
 end
 
  my_all?(array){collection| collection.length <=3}
